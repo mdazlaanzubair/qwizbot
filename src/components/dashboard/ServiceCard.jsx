dd79f5ca29e8724181264a6fc796916de74f6fae
mdazlaanzubair <mdazlaan1996@gmail.com>
2023-03-11 16:55:02 +0500

    blur in active services

--- src/components/dashboard/ServiceCard.jsx
+++ src/components/dashboard/ServiceCard.jsx
@@ -2,7 +2,11 @@ import { Link } from "react-router-dom";
 
 const ServiceCard = ({ service, index }) => {
   return (
-    <div className="card border border-neutral-focus border-opacity-5 hover:bg-neutral hover:text-neutral-content hover:shadow-xl hover:shadow-neutral transition-all ease-in-out duration-300">
+    <div
+      className={`card border border-neutral-focus border-opacity-5 hover:bg-neutral hover:text-neutral-content hover:shadow-xl hover:shadow-neutral transition-all ease-in-out duration-300 ${
+        service.url !== "/qna" ? "blur-sm" : ""
+      }`}
+    >
       <div className="card-head text-center pt-5">
         <div className="avatar placeholder">
           <div className="bg-base-300 text-base-content rounded-full w-12">
@@ -21,9 +25,13 @@ const ServiceCard = ({ service, index }) => {
           ))}
         </p>
         <div className="card-actions justify-end">
-          <Link to={service?.url} className="btn btn-sm btn-ghost">
-            Try it out!
-          </Link>
+          {service.url === "/qna" ? (
+            <Link to={service?.url} className="btn btn-sm btn-ghost">
+              Try it out!
+            </Link>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
