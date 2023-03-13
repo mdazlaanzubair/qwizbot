5524e2361fde63a57cb25861c4f7cacdb4e0b4a4
mdazlaanzubair <mdazlaan1996@gmail.com>
2023-03-13 21:21:10 +0500

    home page completed

---
+++ src/components/home/Animator.jsx
@@ -0,0 +1,8 @@
+import Lottie from "lottie-react";
+import qwizbotAnimation from "../../assets/qwizbot_animation.json";
+
+const Animator = () => {
+  return <Lottie animationData={qwizbotAnimation} />;
+};
+
+export default Animator;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,35 @@
+import { Link } from "react-router-dom";
+import Animator from "../components/home/Animator";
+import { useAuth } from "../contexts/AuthContext";
+
 const Home = () => {
-  return <div>Landing Page</div>;
+  const { user } = useAuth();
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse px-10 text-center lg:text-left">
+        <Animator />
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+
+          {user ? (
+            <Link to="/dashboard" className="btn btn-neutral">
+              Go to Dashboard
+            </Link>
+          ) : (
+            <Link to="/login" className="btn btn-primary">
+              Get Started
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
